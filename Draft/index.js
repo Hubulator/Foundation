f6d30fc08845800f6277a29f5735db11ba3d90c7
Hubulator <GraceAnnaAkparanta@gmail.com>
2025-08-29 20:51:17 -0400

    comment code from front matter

--- Draft/index.js
+++ Draft/index.js
@@ -4,3 +4,50 @@ logger(adder(1, 2));
 
 // printLine();
 
+
+
+
+// <!DOCTYPE html>
+// <html lang="en">
+// <head>
+//     <meta charset="UTF-8">
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//     <title>Foundation</title>
+//     <link rel="stylesheet" href="style.css">
+//     <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet">
+
+//     <!-- <style>
+//         /* This is Internal  */
+//         body{
+//             background-color: rgb(248, 178, 248);
+//         }
+//         h1{
+//             color:rgb(203, 246, 231);
+//         }
+//         p{
+//             background-color:antiquewhite;
+//             color:blueviolet;
+//         }
+
+//     </style> -->
+// </head>
+// This is Inline : <body style = "background-color: black;"> 
+// <body>
+//     <header>
+//         <h1>Header</h1>
+//         <nav><h2><Nav>Nav</Nav></h2></nav>
+
+//     </header>
+
+//     <main>
+//         <h2><Main>Main</Main></h2>
+//     </main>
+
+//     <footer>
+//         <h2>Footer</h2>
+//     </footer>
+
+    
+// </body>
+// </html>
+
